feae2bdf451b7cd0b92c0a580e866a9737901838
JayavardhanYerubandi <143635148+JayavardhanYerubandi@users.noreply.github.com>
2025-03-09 12:01:04 +0530

    app.py

--- app.py
+++ app.py
@@ -76,4 +76,4 @@ def get_random_quote():
     return jsonify(random.choice(quotes))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
